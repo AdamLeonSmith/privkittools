25808e02aa49162396f6d43cc6f76efdb9fb03e8
adam <adam@wearedragonfly.co>
2020-04-25 16:30:21 +0100

    updated analysis script to use better lat/long method

--- analyse_metres_from_location.py
+++ analyse_metres_from_location.py
@@ -4,13 +4,19 @@ import json
 from datetime import datetime, date, timedelta
 
 
+#Enter your longitude here and copy the Length of a Degree of Latittude/Longitude in Metres
+lod_lat = 111061.57849146728
+lod_long = 83627.11630547515
+lat_per_metre = 1/lod_lat
+long_per_metre = 1/lod_long
+
 with open('/home/adam/Desktop/1587822189712.json', encoding='utf-8-sig') as json_file:
   data = json.loads(json_file.read().replace('\n', ''))
 
 tgt_location = {}
 tgt_location['latitude'] = 41.39792327
 tgt_location['longitude'] = 2.15831900
-tgt_time = datetime.fromtimestamp(1587794453000/1000)
+tgt_time = datetime.fromtimestamp(1587808852000/1000)
 start_time = tgt_time - timedelta(hours=4)
 end_time = tgt_time + timedelta(hours=4)
 print("Start of window: " + str(start_time))
@@ -25,4 +31,4 @@ for pt in data:
         if lat_diff < 0.0002:
             dt = datetime.fromtimestamp(pt['time'] / 1000.0)
             if dt > start_time and dt < end_time:
-                print("Point of concern identified... " + str(long_diff / 0.00001) + "m & " + str(lat_diff / 0.00001) +"m & " + str(dt - tgt_time))
+                print("Point of concern identified... " + str(long_diff / long_per_metre) + "m & " + str(lat_diff / lat_per_metre) +"m & " + str(dt - tgt_time))
