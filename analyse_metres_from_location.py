b886e295a6ca6e545f24ffdbb1674f0a23a2342f
adam <adam@wearedragonfly.co>
2020-04-25 16:34:57 +0100

    Improved method

--- analyse_metres_from_location.py
+++ analyse_metres_from_location.py
@@ -14,7 +14,7 @@ with open('/home/adam/Desktop/1587822189712.json', encoding='utf-8-sig') as json
   data = json.loads(json_file.read().replace('\n', ''))
 
 tgt_location = {}
-tgt_location['latitude'] = 41.39792327
+tgt_location['latitude'] = 41.3979457
 tgt_location['longitude'] = 2.15831900
 tgt_time = datetime.fromtimestamp(1587808852000/1000)
 start_time = tgt_time - timedelta(hours=4)
@@ -27,8 +27,8 @@ hits = 0
 for pt in data:
     long_diff = abs(tgt_location['longitude'] - pt['longitude'])
     lat_diff = abs(tgt_location['latitude'] - pt['latitude'])
-    if long_diff < 0.0002:
-        if lat_diff < 0.0002:
+    if long_diff < (20 * long_per_metre):
+        if lat_diff < (20 * lat_per_metre):
             dt = datetime.fromtimestamp(pt['time'] / 1000.0)
             if dt > start_time and dt < end_time:
                 print("Point of concern identified... " + str(long_diff / long_per_metre) + "m & " + str(lat_diff / lat_per_metre) +"m & " + str(dt - tgt_time))
